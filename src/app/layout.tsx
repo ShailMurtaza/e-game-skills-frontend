f57064996b337b40e3875a1690fee3b3aa5ccce1
ShailMurtaza <shailmurtaza@gmail.com>
2025-10-11 15:01:22 +0500

    Navbar, Homepage, Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,54 @@
+import { Play, Orbitron, Russo_One } from "next/font/google";
 import type { Metadata } from "next";
 import "./globals.css";
+import Link from "next/link";
+
+const play = Play({
+    subsets: ["latin"],
+    weight: ["400", "700"], // optional: choose the weights you need
+});
 
 export const metadata: Metadata = {
-  title: "e Game Skills",
-  description: "e Game Skills",
+    title: "e Game Skills",
+    description: "e Game Skills",
 };
-
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+    const year = new Date().getFullYear();
+    return (
+        <html lang="en">
+            <body>
+                <nav
+                    className={`${play.className} text-lg font-bold flex flex-row p-7 rounded-b-3xl shadow-[0_4px_30px_rgba(131,206,239,0.7)] bg-black/70 backdrop-blur-md fixed w-full z-10`}
+                >
+                    <ul className="flex flex-row gap-5">
+                        <li>
+                            <Link href="/">E Game Skills</Link>
+                        </li>
+                        <li>
+                            <Link href="/">About Us</Link>
+                        </li>
+                        <li>
+                            <Link href="/">Contact Us</Link>
+                        </li>
+                    </ul>
+                    <ul className="flex flex-row gap-5 ml-auto">
+                        <li>
+                            <button>Sign In</button>
+                        </li>
+                        <li>
+                            <button>Sign Up</button>
+                        </li>
+                    </ul>
+                </nav>
+                <div>{children}</div>
+                <footer className="mt-5 p-5 text-center">
+                    &copy; {year} E Game Skills, all rights reserved.
+                </footer>
+            </body>
+        </html>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,40 @@
+"use client";
+import { motion } from "framer-motion";
+import { Orbitron } from "next/font/google";
+
+const orbitron = Orbitron({ subsets: ["latin"], weight: ["500", "700"] });
+
 export default function Home() {
-  return <h1>e Game Skills</h1>;
+    return (
+        <main className={orbitron.className}>
+            <section
+                className="flex flex-row items-center justify-center relative bg-center bg-cover bg-no-repeat min-h-[764px]"
+                style={{ backgroundImage: "url('/radiant_dire.jpg')" }}
+            >
+                <motion.div
+                    className="text-5xl font-bold absolute bottom-1/12 z-0"
+                    initial={{ opacity: 0, y: 150 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    viewport={{ once: true, amount: 0.4 }}
+                    transition={{ duration: 0.8, ease: "easeOut" }}
+                >
+                    Let's dive into the world of eSports
+                </motion.div>
+            </section>
+            <section
+                className="flex flex-row justify-center mt-10 bg-center relative bg-cover bg-no-repeat min-h-[764px]"
+                style={{ backgroundImage: "url('/stadium.jpg')" }}
+            >
+                <motion.div
+                    className="text-5xl font-bold absolute bottom-1/12 z-0"
+                    initial={{ opacity: 0, y: 150 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    viewport={{ once: false, amount: 0.4 }}
+                    transition={{ duration: 0.8, ease: "easeOut" }}
+                >
+                    Pursue Your Dream To Become An eSports Champoin
+                </motion.div>
+            </section>
+        </main>
+    );
 }
