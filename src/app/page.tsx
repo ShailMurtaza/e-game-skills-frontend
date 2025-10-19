2f7fffb50289b8d1cee00caa61c16047bc5189c6
ShailMurtaza <shailmurtaza@gmail.com>
2025-10-19 23:46:29 +0500

    Created standard for headings in globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,20 +12,20 @@ export default function Home() {
                 className="flex flex-row items-center justify-center relative bg-center bg-cover bg-no-repeat h-screen"
                 style={{ backgroundImage: "url('/radiant_dire.jpg')" }}
             >
-                <motion.div
+                <motion.h1
                     className="text-5xl font-bold absolute bottom-1/12 z-0"
                     initial={{ opacity: 0, y: 150 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.8, ease: "easeOut" }}
                 >
                     Let's dive into the world of eSports
-                </motion.div>
+                </motion.h1>
             </section>
             <section
                 className="flex flex-row justify-center mt-10 bg-center relative bg-cover bg-no-repeat h-screen"
                 style={{ backgroundImage: "url('/stadium.jpg')" }}
             >
-                <motion.div
+                <motion.h1
                     className="text-5xl font-bold absolute bottom-1/12 z-0"
                     initial={{ opacity: 0, y: 150 }}
                     whileInView={{ opacity: 1, y: 0 }}
@@ -33,7 +33,7 @@ export default function Home() {
                     transition={{ duration: 0.8, ease: "easeOut" }}
                 >
                     Pursue Your Dream To Become An eSports Champoin
-                </motion.div>
+                </motion.h1>
             </section>
             <section className="mt-10">
                 <FAQ />
