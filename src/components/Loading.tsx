11a87cd82b8669f5235cd5131b8633897e6fdb94
ShailMurtaza <shailmurtaza@gmail.com>
2025-11-01 14:38:10 +0500

    Loading made better

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -4,10 +4,14 @@ import { useUI } from "@/context/UIContext";
 export default function Loading() {
     const { loading } = useUI();
     if (!loading) return null;
+    return <LoadingComponent />;
+}
 
+export function LoadingComponent() {
     return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black/50 text-white text-lg z-50">
-            Loading...
+        <div className="fixed inset-0 flex items-center justify-center min-h-screen bg-black/50 z-50">
+            <div className="animate-spin rounded-full h-45 w-45 border-t-10 border-b-5 border-blue-500"></div>
+            <div className="absolute">Loading...</div>
         </div>
     );
 }
