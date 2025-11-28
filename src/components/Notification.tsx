e9cfc137d8423fac67678f954becd9aa8a699688
ShailMurtaza <shailmurtaza@gmail.com>
2025-11-28 15:34:30 +0500

    Notification: z-60

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -10,7 +10,7 @@ export default function Notification() {
 
     return (
         <div
-            className={`${color} fixed top-4 right-4 text-white px-4 py-2 rounded-lg shadow-md z-50`}
+            className={`${color} fixed top-4 right-4 text-white px-4 py-2 rounded-lg shadow-md z-60`}
         >
             {notification.message}
         </div>
